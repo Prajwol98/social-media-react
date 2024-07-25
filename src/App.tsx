f2c44eb6e6de20de934f56e248c426c45efb7b83
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-25 17:57:58 +0545

    added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import SignInForm from "./_auth/form/SignInForm";
+import SignupForm from "./_auth/form/SignupForm";
+import { Home } from "./_root/pages";
+
 import "./global.css";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* {public route} */}
+        <Route path="/sign-In" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* {PRivate Route} */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/form/SignInForm.tsx
@@ -0,0 +1,5 @@
+const SignInForm = () => {
+  return <div>this is the sign in form that we will develop in the future</div>;
+};
+
+export default SignInForm;
