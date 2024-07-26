43b5b6250ffb1324e98895a921065e83f94380c7
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-26 15:22:07 +0545

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import AuthLayout from "./_auth/AuthLayout";
 import SignInForm from "./_auth/form/SignInForm";
 import SignupForm from "./_auth/form/SignupForm";
 import { Home } from "./_root/pages";
-
+import RootLayout from "./_root/RootLayout";
 import "./global.css";
 import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* {public route} */}
-        <Route path="/sign-In" element={<SignInForm />} />
-        <Route path="/sign-up" element={<SignupForm />} />
+        {/* {Public Route} */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-In" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-        {/* {PRivate Route} */}
-        <Route path="/home" element={<Home />} />
+        {/* {Private Route} */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
