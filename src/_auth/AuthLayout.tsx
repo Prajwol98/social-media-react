6e67af164081a381094411a16d4f42262445238f
Prajwol Mainali <prajwolmainali96@gmail.com>
2024-07-26 21:04:29 +0545

    updates

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -2,15 +2,23 @@ import { Outlet, Navigate } from "react-router-dom";
 const AuthLayout = () => {
   const isAuthenticated = false;
   return (
-    <div>
+    <>
       {isAuthenticated ? (
-        <Navigate to="/"></Navigate>
+        <Navigate to="/" />
       ) : (
-        <section>
-          <Outlet></Outlet>
-        </section>
+        <>
+          <section className="flex items-center justify-center py-10 flex-col flex-1">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="img"
+            className="hidden xl:block object-cover bg-no-repeat h-screen w-1/2"
+          />
+        </>
       )}
-    </div>
+    </>
   );
 };
 
--- src/_auth/form/SignupForm.tsx
+++ src/_auth/form/SignupForm.tsx
@@ -1,9 +1,121 @@
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+
+// const formSchema = z.object({
+//   username: z.string().min(2, {
+//     message: "Username must be at least 2 characters.",
+//   }),
+// });
+
+// const SignupForm = () => {
+//   return (
+//     <div>
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//           <FormField
+//             control={form.control}
+//             name="username"
+//             render={({ field }) => (
+//               <FormItem>
+//                 <FormLabel>Username</FormLabel>
+//                 <FormControl>
+//                   <Input placeholder="shadcn" {...field} />
+//                 </FormControl>
+//                 <FormDescription>
+//                   This is your public display name.
+//                 </FormDescription>
+//                 <FormMessage />
+//               </FormItem>
+//             )}
+//           />
+//           <Button type="submit">Submit</Button>
+//         </form>
+//       </Form>
+//     </div>
+//   );
+// };
+
+// export default SignupForm;
+
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+
+// const formSchema = z.object({
+//   username: z.string().min(2, {
+//     message: "Username must be at least 2 characters.",
+//   }),
+// });
+
+// const SignupForm = () => {
+//   const form = useForm({
+//     resolver: zodResolver(formSchema),
+//   });
+
+//   const onSubmit = (data) => {
+//     console.log(data);
+//   };
+
+//   return (
+//     <div>
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//           <FormField
+//             control={form.control}
+//             name="username"
+//             render={({ field }) => (
+//               <FormItem>
+//                 <FormLabel>Username</FormLabel>
+//                 <FormControl>
+//                   <Input placeholder="shadcn" {...field} />
+//                 </FormControl>
+//                 <FormDescription>
+//                   This is your public display name.
+//                 </FormDescription>
+//                 <FormMessage />
+//               </FormItem>
+//             )}
+//           />
+//           <Button type="submit">Submit</Button>
+//         </form>
+//       </Form>
+//     </div>
+//   );
+// };
+
+// export default SignupForm;
 import { Button } from "@/components/ui/button";
+import React from "react";
 
 const SignupForm = () => {
   return (
     <div>
-      <Button>click me</Button>
+      <Button>click me this is sign up</Button>
     </div>
   );
 };
